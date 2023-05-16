4fe411826e5714588b6c956d8a81707d86bf25db
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2023-05-16 00:45:42 +0000

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,23 @@
+
+import scapy.all as scapy
+
+request = scapy.ARP()
+
+request.pdst = '192.168.1.1/24'
+broadcast = scapy.Ether()
+
+broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+
+request_broadcast = broadcast / request
+clients = scapy.srp(request_broadcast, timeout = 1)[0]
+
+print("")
+print(" ____________________________")
+print("|-Nueva IP----|----Nueva MAC-|")
+print("l-------------|--------------j")
+print("")
+for element in clients:
+    print(element[1].psrc + " | " + element[1].hwsrc)
+print("")
+print("Escaneo de red finalizado...")
+
